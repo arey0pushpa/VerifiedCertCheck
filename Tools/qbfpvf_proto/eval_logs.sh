1ae3874450a987b77048566ea1df86f7da76ac5d
Peter Lammich <plammich@gmail.com>
2020-05-09 11:43:38 +0100

    evak logs

---
+++ Tools/qbfpvf_proto/eval_logs.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+function error() {
+  echo "Error: $1"
+  exit 1
+}
+
+
+test $# -eq 1 || error "Usage: eval-logs log-dir"
+
+LOGDIR=$1
+
+
+
+egrep "User time|Trace size|Proof size|^s" "$LOGDIR"/*.log\
+  | sed -re 's/:/ /;s/Trace size:/trace-size/;s/Proof size:/proof-size/;s/User time.*:/user-time/;
+             s|^.*/||; s/\.log / /; s/\.(solve|reduce|verify)/ \1/
+            '\
+  | gawk -f <(cat <<'HERE'
+      {
+        problem=$1
+        cat=$2 "-" $3
+        value=$4
+        data[problem][cat] = value
+      }
+
+      END {
+        n = split("solve-user-time solve-trace-size reduce-user-time reduce-proof-size verify-user-time verify-s",cats)
+
+        printf "problem "
+        for (i=1;i<n;i++) printf "%s, ", cats[i];
+        print cats[n]
+
+        PROCINFO["sorted_in"] = "@ind_str_asc"
+
+        for (p in data) {
+          printf "%s, ", p
+
+          for (i=1;i<n;i++) printf "%s, ", data[p][cats[i]];
+          print data[p][cats[i]]
+
+
+        }
+      }
+
+HERE
+)
